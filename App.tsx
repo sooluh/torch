ff6568418fa088a7ec6149307132f8e002c38133
Abu Masyail <20874779+sooluh@users.noreply.github.com>
2023-08-05 14:55:55 +0700

    feat: host to lanyard instead of supabase

--- App.tsx
+++ App.tsx
@@ -1,37 +1,16 @@
 import "react-native-url-polyfill/auto";
 import * as Location from "expo-location";
 import { StatusBar } from "expo-status-bar";
-import * as SecureStore from "expo-secure-store";
 import * as TaskManager from "expo-task-manager";
 import React, { useState, useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const LOCATION_TRACKING = "location-tracking";
-const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const SUPABASE_ANON = process.env.EXPO_PUBLIC_SUPABASE_ANON;
+const DISCORD_USER_ID = process.env.EXPO_PUBLIC_DISCORD_USER_ID!;
+const LANYARD_API_KEY = process.env.EXPO_PUBLIC_LANYARD_API_KEY!;
 
-const ExpoSecureStoreAdapter = {
-  getItem: (key) => {
-    return SecureStore.getItemAsync(key);
-  },
-  setItem: (key, value) => {
-    SecureStore.setItemAsync(key, value);
-  },
-  removeItem: (key) => {
-    SecureStore.deleteItemAsync(key);
-  },
-};
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON, {
-  auth: {
-    storage: ExpoSecureStoreAdapter,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
-});
+console.log(DISCORD_USER_ID, LANYARD_API_KEY)
 
 const styles = StyleSheet.create({
   container: {
@@ -43,8 +22,24 @@ const styles = StyleSheet.create({
   },
 });
 
+const update = async (lat: number, lon: number) => {
+  const reverse = `https://geocode.maps.co/reverse?lat=${lat}&lon=${lon}`;
+  const lanyard = `https://api.lanyard.rest/v1/users/${DISCORD_USER_ID}/kv/location`;
+
+  const json = await fetch(reverse).then((res) => res.json());
+  const location = [json.address.town, json.address.country_code.toUpperCase()].join(", ");
+
+  const response = await fetch(lanyard, {
+    method: "PUT",
+    body: location,
+    headers: { Authorization: LANYARD_API_KEY },
+  });
+
+  console.log(response);
+};
+
 export default function App() {
-  const [status, setStatus] = useState("Isn't it cool first?");
+  const [status, setStatus] = useState<string>("Isn't it cool first?");
 
   const initialize = async () => {
     const foreground = await Location.requestForegroundPermissionsAsync();
@@ -91,27 +86,21 @@ export default function App() {
   );
 }
 
-TaskManager.defineTask(LOCATION_TRACKING, async ({ data, error }) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
+TaskManager.defineTask<{ locations: Location.LocationObject[] }>(
+  LOCATION_TRACKING,
+  async ({ data, error }) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
 
-  if (data) {
-    const [current] = data.locations;
-    const lat = current.coords.latitude;
-    const lng = current.coords.longitude;
+    if (data) {
+      const [current] = data.locations;
+      const lat = current.coords.latitude;
+      const lon = current.coords.longitude;
 
-    await supabase
-      .from("settings")
-      .update({ value: lat })
-      .eq("key", "latitude");
-
-    await supabase
-      .from("settings")
-      .update({ value: lng })
-      .eq("key", "longitude");
-
-    console.log(`${new Date(Date.now()).toLocaleString()}: ${lat},${lng}`);
-  }
-});
+      await update(lat, lon);
+      console.log(`${new Date(Date.now()).toLocaleString()}: ${lat},${lon}`);
+    }
+  },
+);
